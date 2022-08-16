8fddcaae034daf86952bc035c65ab9e902ae8f88
jonatan soria <jonatansoriatw@gmail.com>
2022-08-15 21:03:29 -0300

    primer entrega

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          primer entrega de mystore-jonatan soria.
         </p>
         <a
           className="App-link"
